235e1aa7ad06c51be7a2d4a4e0f584c618f81d5b
João Victor Rodrigues <jvpr123@outlook.com>
2022-07-26 22:20:20 -0300

    feat: try to implement in a simple way contract methods

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,43 +1,54 @@
 import { useState, useEffect, useCallback, useMemo } from "react";
 import Web3 from "web3";
 
+import LubyGameContract from "./contracts/LubyGame.json";
+
 function App() {
+  const web3 = new Web3(Web3.givenProvider);
+
   const [account, setAccoount] = useState();
-  const [network, setNetwork] = useState();
-  const [balance, setBalance] = useState();
-
-  const web3 = useMemo(() => new Web3(Web3.givenProvider), []);
-
-  const loadAccounts = useCallback(async (provider) => {
-    const accounts = await provider.eth.requestAccounts();
-    setAccoount(accounts[0]);
-  }, []);
-
-  const loadBalance = useCallback(
-    async (provider) => {
-      const network = await provider.eth.net.getNetworkType();
-      const balance = await provider.eth.getBalance(account);
-
-      setNetwork(network);
-      setBalance(balance);
-    },
-    [account]
-  );
-
-  window.ethereum.on("accountsChanged", (accounts) => setAccoount(accounts[0]));
-  window.ethereum.on("chainChanged", () => loadBalance(web3));
+  const [contract, setContract] = useState();
 
   useEffect(() => {
+    const loadAccounts = async (provider) => {
+      const accounts = await provider.eth.requestAccounts();
+      setAccoount(accounts[0]);
+    };
+
+    const loadContract = async (provider) => {
+      const networkId = await provider.eth.net.getId();
+      const contract = new provider.eth.Contract(
+        LubyGameContract.abi,
+        LubyGameContract.networks[networkId] &&
+          LubyGameContract.networks[networkId].address
+      );
+
+      setContract(contract);
+    };
+
     loadAccounts(web3);
-    loadBalance(web3);
-  }, [loadAccounts, loadBalance, web3]);
+    loadContract(web3);
+  }, []);
+
+  const handleMint = async () =>
+    await contract.methods.mintLbc(10).send({ from: account });
+
+  const handleBalance = async () => {
+    try {
+      await contract.methods.startGame(1).send({ from: account });
+
+      const bal = await contract.methods.getBalanceIndividual().call();
+
+      console.log(bal);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <>
-      <p>Account: {account}</p>
-      <p>
-        Balance ({network}): {balance / (1e18).toFixed(5)}
-      </p>
+      <button onClick={handleMint}>Mint</button>
+      <button onClick={handleBalance}>Balance</button>
     </>
   );
 }
