66460142d8e276d35bfb0295aaee3d814c5df0cf
João Victor Rodrigues <jvpr123@outlook.com>
2022-07-28 22:09:40 -0300

    feat: correct last commit (remaining files)

---
+++ client/src/components/modal/InstallMetamaskModal.jsx
@@ -0,0 +1,48 @@
+import { useState, useEffect } from "react";
+
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+
+const InstallMetamaskModal = ({ toggle }) => {
+  const [show, setShow] = useState(false);
+
+  const handleShow = () => setShow(true);
+  const handleClose = () => setShow(false);
+
+  const handleRedirect = () => {
+    handleShow(false);
+    window.open("https://metamask.io/", "Metamask", "height=500,width=500");
+  };
+
+  useEffect(() => {
+    toggle ? handleShow() : handleClose();
+  }, [toggle]);
+
+  return (
+    <Modal show={show} centered={true} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>Metamask extension required!</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <p>
+          To play this game you need Metamask to manage your wallets and
+          establish a connection to the Ethereum network.
+        </p>
+        <p>
+          Please, install the extension and then click "Connect to Metamask" to
+          try again.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button variant="secondary" onClick={handleClose}>
+          Close
+        </Button>
+        <Button variant="primary" onClick={handleRedirect}>
+          Install Metamask
+        </Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export default InstallMetamaskModal;
